ff02cd52e32ce357f42f0c505f64e6a0b83ef60b
toni <tonigstm96@gmail.com>
2026-06-16 16:53:21 +0200

    feat: added alert to form submit with sweetalerts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,7 +7,6 @@ module.exports = {
       path.relative(process.cwd(), file),
     );
 
-    // Obtener UID y GID en Linux para evitar conflictos de permisos de root (sin comillas literales)
     const uid = process.getuid ? process.getuid() : 1000;
     const gid = process.getgid ? process.getgid() : 1000;
     const cwd = process.cwd();
